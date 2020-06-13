364d10968a65b6c67083b4b4b9361dbfc3dbde3f
GabrielK <53771946+GabrielKotz@users.noreply.github.com>
2020-06-12 21:17:14 -0300

    Update app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UsuarioController } from './usuario.controller';
+import { UsuarioService } from './usuario.service';
+
+
+
+@Module({
+  imports: [
+  ],
+  controllers: [
+    UsuarioController
+  ],
+  providers: [
+      
+ UsuarioService
+   
+  ],
+})
+export class AppModule { }
+
